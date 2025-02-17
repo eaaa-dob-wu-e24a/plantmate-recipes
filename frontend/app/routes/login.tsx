f0eb1bc1ba929f3fab17f7821c48c728a9e68095
madsnbkristensen <madskristensen@hotmail.dk>
2025-02-17 16:25:37 +0100

    Added btn component

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,9 +1,12 @@
 import { Form } from "react-router";
+import { Button } from "~/components/ui/button";
 
 export default function Login() {
   return (
     <div className="flex justify-center items-center w-full h-full">
-      <Form className="w-full max-w-sm"></Form>
+      <Form className="w-full max-w-sm">
+        <Button variant="default">Login</Button>
+      </Form>
     </div>
   );
 }
