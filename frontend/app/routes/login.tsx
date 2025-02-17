2d1264aa82fa482ced2d56681777ccb231b8c188
madsnbkristensen <madskristensen@hotmail.dk>
2025-02-17 14:05:37 +0100

    Login route init

---
+++ frontend/app/routes/login.tsx
@@ -0,0 +1,9 @@
+import { Form } from "react-router";
+
+export default function Login() {
+  return (
+    <div className="flex justify-center items-center w-full h-full">
+      <Form className="w-full max-w-sm"></Form>
+    </div>
+  );
+}
