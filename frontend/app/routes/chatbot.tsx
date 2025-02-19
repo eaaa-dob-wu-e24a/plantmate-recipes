e919b597812e26d1277a4767f4be8ed7a2c90d0d
madsnbkristensen <madskristensen@hotmail.dk>
2025-02-19 15:57:30 +0100

    started refactoring chatbot to use Action hook

--- frontend/app/routes/chatbot.tsx
+++ frontend/app/routes/chatbot.tsx
@@ -2,11 +2,28 @@ import { useState } from "react";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
-import { Home, Bookmark, User } from "lucide-react";
+import type { Route } from "./+types/chatbot";
+import { Form } from "react-router";
+import { useFetcher } from "react-router";
 
 export const loader = async () => {
   return null;
 };
+
+export async function action({ params, request }: Route.ActionArgs) {
+  let formData = Object.fromEntries(await request.formData());
+  try {
+    const response = await fetch(process.env.API_URL + "/recipies", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 // Placeholder for message interface til chatbotten.
 interface Message {
@@ -15,20 +32,8 @@ interface Message {
 }
 
 const Chatbot: React.FC = () => {
-  const [messages, setMessages] = useState<Message[]>([
-    {
-      text: "Hey, welcome back to Plant Mate! Let's chat.",
-      sender: "bot",
-    },
-  ]);
-  const [input, setInput] = useState<string>("");
-
-  const handleSend = () => {
-    if (input.trim() !== "") {
-      setMessages([...messages, { text: input, sender: "user" }]);
-      setInput("");
-    }
-  };
+  const [messages, setMessages] = useState<Message[]>([]);
+  const fetcher = useFetcher();
 
   return (
     <div className="flex flex-col justify-between h-full">
@@ -38,13 +43,15 @@ const Chatbot: React.FC = () => {
             key={index}
             className={`flex ${
               msg.sender === "user" ? "justify-end" : "justify-start"
-            }`}>
+            }`}
+          >
             <Card
               className={`max-w-[75%] p-3 rounded-xl shadow-sm ${
                 msg.sender === "user"
                   ? "bg-[var(--primary-green)] text-[var(--primary-white)] rounded-br-none"
                   : "bg-[var(--grey-color)] text-[var(--black-color)] rounded-bl-none"
-              }`}>
+              }`}
+            >
               <CardContent className="p-0">{msg.text}</CardContent>
             </Card>
           </div>
@@ -52,22 +59,17 @@ const Chatbot: React.FC = () => {
       </div>
 
       <div className="flex relative bottom-0 items-center gap-2 p-4 bg-[var(--primary-white)]">
-        <Input
-          className="flex-1 bg-[#E6E2D8] p-3 rounded-xl border-none focus:ring-0 text-sm"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSend();
-            }
-          }}
-          placeholder="Message lil Miso Homie"
-        />
-        <Button
-          className="bg-[var(--primary-green)] text-[var(--primary-white)] rounded-xl px-4 py-2 text-sm"
-          onClick={handleSend}>
-          Send
-        </Button>
+        <fetcher.Form method="post" className="flex w-full gap-2">
+          <Input
+            className="flex-1 bg-[#E6E2D8] p-3 rounded-xl border-none focus:ring-0 text-sm"
+            type="text"
+            name="message"
+            placeholder="Message lil Miso Homie"
+          />
+          <Button className="bg-[var(--primary-green)] text-[var(--primary-white)] rounded-xl px-4 py-2 text-sm">
+            Send
+          </Button>
+        </fetcher.Form>
       </div>
     </div>
   );
