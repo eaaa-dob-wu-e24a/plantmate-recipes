815ba124806e9fd5da3f0b603985245533e6023e
madsnbkristensen <madskristensen@hotmail.dk>
2025-02-19 12:57:29 +0100

    Small css adjustments

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -1,6 +1,13 @@
 import { Home, Bookmark, User } from "lucide-react";
+import { useLocation } from "react-router";
 
 export default function Footer() {
+  const location = useLocation();
+
+  if (location.pathname === "/login") {
+    return null;
+  }
+
   return (
     <div className="flex justify-around items-center py-3 border-t border-[#E6E2D8] bg-[var(--primary-white)]">
       <Home className="w-6 h-6 text-[var(--primary-green)]" />
