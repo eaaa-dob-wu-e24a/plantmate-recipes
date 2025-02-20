0d70a18a948fe89213d8bd2b8bed88481c93ee3a
EmilDalhoff <emildalhoff@hotmail.com>
2025-02-20 12:36:16 +0100

    styling

--- frontend/app/components/logo.tsx
+++ frontend/app/components/logo.tsx
@@ -4,12 +4,9 @@ import { WifiHigh } from "lucide-react";
 export default function Logo() {
   return (
     <div className="relative w-full text-black">
-      {/* iPhone Status Bar Container */}
       <div className="h-8 relative">
-        {/* “Notch” */}
         <div className="bg-black w-24 h-5 rounded-b-xl absolute left-1/2 -translate-x-1/2 top-0" />
 
-        {/* Time + Icons */}
         <div className="flex justify-between items-center px-3 absolute top-0.5 left-0 right-0">
           <span className="px-2 pt-2 font-medium">9:41</span>
           <div className="flex space-x-1">
@@ -19,7 +16,6 @@ export default function Logo() {
         </div>
       </div>
 
-      {/* Logo Container */}
       <div className="flex  justify-center items-center p-6  bg-[var(--primary-white)] rounded-b-2xl space-x-2">
         <img src="/pmlogo.svg" alt="Plant Mate" className="h-8 w-auto" />
       </div>
